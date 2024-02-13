f91908f89c0b9371125204e1de206859be833491
francescomarsilio <157403558+francescomarsilio@users.noreply.github.com>
2024-02-13 13:48:51 +0100

    Create port_scanner.py
    Sviluppo base di un Port scanner con Python

---
+++ Python_Port_Scanner/port_scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+obiettivo_ip =  input("Inserire l'indirizzo IP da sannerizzare: ") #chiediamo all'utente di inserire l'idirizzo IP
+intervallo_porte = input("Inserisci il range di porte da scannerizzare (es:0-65535): ") #chiediamo all'utente di inserire il range di porte
+
+porta_bassa = int(intervallo_porte.split("-")[0]) #estrae i valori minimi delle porte dall'input dell'utente
+porta_alta = int(intervallo_porte.split("-")[1]) #estrae i vaolri massimi delle porte dall'input dell utente
+
+print(f"Scansione host ", {obiettivo_ip}, "da porta", {porta_bassa}, "a porta", {porta_alta})
+
+for porta in range(porta_bassa, porta_alta):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    status = s.connect_ex((obiettivo_ip, porta)) #tentativo di connessione alla porta indicata
+    if status == 0: #se la connessione ha successo vuol dire che la porta è aperta
+        print("*** Port", porta, "- APERTA ***")
+        # se non restituisce niente vuol dire che la porta è chiusa
+
+    s.close()
