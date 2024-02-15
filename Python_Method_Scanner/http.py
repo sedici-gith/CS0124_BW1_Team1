c4f3d5caa0b2661510274d0b49f264a35c2a25f4
Verdiana Germani <germani.verdiana@gmail.com>
2024-02-15 14:50:58 +0100

    revision method http.py
    Revisionato da D'Esposito, implementato da Germani e Mineo

--- Python_Method_Scanner/http.py
+++ Python_Method_Scanner/http.py
@@ -2,32 +2,31 @@ import http.client
 def main():
     IP = input("inserire IP target: ")
     porta = input("inserire porta target (default: 80): ")
-    percorso = input("ïnserire percorso (default: /): ")
+    percorso = input("inserire percorso (default: /): ")
     metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
 
     if porta == "":
         porta = 80
-        
+       
     if percorso == "":
         percorso = "/"
+    scansione(IP, porta, percorso, metodi)
 
-    def scansione():
-        for metodo in metodi:
-            connection = http.client.HTTPConnection(IP, porta)
-            connection.request(metodo, percorso)
-            risposta = connection.getresponse()
-            if risposta.status < 400:
-                print(f"metodo {metodo} abilitato, codice {risposta.status}")
-            else:
-                print(f"metodo {metodo} disabilitato, codice {risposta.status}")
-        print(IP, "/", porta, "/", percorso)
-    
-    try: 
-        scansione()
-
-    except ConnectionRefusedError:
-        print("connessione fallita")
-        print(IP, "/", porta, "/", percorso)
+def scansione(IP, porta, percorso, metodi):
+	try:
+		for metodo in metodi:
+			connection = http.client.HTTPConnection(IP, porta)
+			connection.request(metodo, percorso)
+			risposta = connection.getresponse()
+			if risposta.status < 400:
+			    print(f"metodo {metodo} abilitato, codice {risposta.status}")
+			else:
+			    print(f"metodo {metodo} disabilitato, codice {risposta.status}")
+		print(IP, "/", porta, "/", percorso)
+	except ConnectionRefusedError:
+		print("connessione fallita")
+		print(IP, "/", porta, "/", percorso)
     
 if __name__ == "__main__":
- main()
+    main()
+
