5dd7aa8e0a4a07204322de7dd2a5d431456af568
francescomarsilio <157403558+francescomarsilio@users.noreply.github.com>
2024-02-15 12:56:42 +0100

    modifica port_scanner.py
    Revisionato da Marsilio in collaborazione con D'Esposito, Germani e Mineo

--- Python_Port_Scanner/port_scanner.py
+++ Python_Port_Scanner/port_scanner.py
@@ -1,18 +1,26 @@
 import socket
 
-obiettivo_ip =  input("Inserire l'indirizzo IP da sannerizzare: ") #chiediamo all'utente di inserire l'idirizzo IP
-intervallo_porte = input("Inserisci il range di porte da scannerizzare (es:0-65535): ") #chiediamo all'utente di inserire il range di porte
+def main():
 
-porta_bassa = int(intervallo_porte.split("-")[0]) #estrae i valori minimi delle porte dall'input dell'utente
-porta_alta = int(intervallo_porte.split("-")[1]) #estrae i vaolri massimi delle porte dall'input dell utente
+    obiettivo_ip =  input("Inserire l'indirizzo IP da sannerizzare: ") #chiediamo all'utente di inserire l'idirizzo IP
+    intervallo_porte = input("Inserisci il range di porte da scannerizzare (es:0-65535): ") #chiediamo all'utente di inserire il range di porte
 
-print(f"Scansione host ", {obiettivo_ip}, "da porta", {porta_bassa}, "a porta", {porta_alta})
+    porta_bassa = int(intervallo_porte.split("-")[0]) #estrae i valori minimi delle porte dall'input dell'utente
+    porta_alta = int(intervallo_porte.split("-")[1]) #estrae i vaolri massimi delle porte dall'input dell utente
 
-for porta in range(porta_bassa, porta_alta):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    status = s.connect_ex((obiettivo_ip, porta)) #tentativo di connessione alla porta indicata
-    if status == 0: #se la connessione ha successo vuol dire che la porta è aperta
-        print("*** Port", porta, "- APERTA ***")
+    print(f"Scansione host", {obiettivo_ip}, "da porta", {porta_bassa}, "a porta", {porta_alta})
+    scanner(porta_bassa, porta_alta, obiettivo_ip)
+
+def scanner(porta_bassa, porta_alta, obiettivo_ip):
+
+    for porta in range(porta_bassa, porta_alta):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        status = s.connect_ex((obiettivo_ip, porta)) #tentativo di connessione alla porta indicata
+        if status == 0: #se la connessione ha successo vuol dire che la porta è aperta
+            print("*** Port", porta, "- APERTA ***")
         # se non restituisce niente vuol dire che la porta è chiusa
 
     s.close()
+
+if __name__ == "__main__":
+    main()
