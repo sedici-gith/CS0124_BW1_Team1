115b87e4264a7eb1eb5f8f92e791f5783fdeaf3b
Francescomineo1999 <157381483+Francescomineo1999@users.noreply.github.com>
2024-02-15 11:32:33 +0100

    Modifica finale http.py
    Revisione di d'Esposito e implementazione di Germani e Mineo

--- Python_Method_Scanner/http.py
+++ Python_Method_Scanner/http.py
@@ -2,28 +2,32 @@ import http.client
 def main():
     IP = input("inserire IP target: ")
     porta = input("inserire porta target (default: 80): ")
-    metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']
+    percorso = input("ïnserire percorso (default: /): ")
+    metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
 
     if porta == "":
         porta = 80
+        
+    if percorso == "":
+        percorso = "/"
 
     def scansione():
         for metodo in metodi:
             connection = http.client.HTTPConnection(IP, porta)
-            connection.request(metodo, "/")
+            connection.request(metodo, percorso)
             risposta = connection.getresponse()
             if risposta.status < 400:
-                print(f"metodo {metodo} abilitato")
+                print(f"metodo {metodo} abilitato, codice {risposta.status}")
             else:
-                print(f"metodo {metodo} disabilitato")
-        print(IP, "/", porta)
+                print(f"metodo {metodo} disabilitato, codice {risposta.status}")
+        print(IP, "/", porta, "/", percorso)
     
     try: 
         scansione()
 
     except ConnectionRefusedError:
         print("connessione fallita")
-        print(IP, "/", porta)
+        print(IP, "/", porta, "/", percorso)
     
 if __name__ == "__main__":
  main()
