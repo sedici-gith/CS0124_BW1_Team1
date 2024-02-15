119bd0ddb27356af43d56872c21e77159e1dc0d1
P1ZZO <157240502+P1ZZO@users.noreply.github.com>
2024-02-15 10:47:22 +0100

    Update http.py
    ho reso il codice piu' generico dando la possibilita' di inserire in input il path specifico e ho reso visibile il codice http di risposta per ogni metodo richiesto

--- Python_Method_Scanner/http.py
+++ Python_Method_Scanner/http.py
@@ -2,20 +2,25 @@ import http.client
 
 IP = input("inserire IP target: ")
 porta = input("inserire porta target (default: 80): ")
-metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']
+percorso = input("ïnserire percorso (default: /): ")
+metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
 
 if porta == "":
     porta = 80
 
+if percorso == "":
+    percorso = "/"
+
+
 def scansione():
     for metodo in metodi:
         connection = http.client.HTTPConnection(IP, porta)
-        connection.request(metodo, "/")
+        connection.request(metodo, percorso)
         risposta = connection.getresponse()
         if risposta.status < 400:
-            print(f"metodo {metodo} abilitato")
+            print(f"metodo {metodo} abilitato, codice {risposta.status}")
         else:
-            print(f"metodo {metodo} disabilitato")
+            print(f"metodo {metodo} disabilitato, codice {risposta.status}")
     print(IP, "/", porta)
     
 try: 
