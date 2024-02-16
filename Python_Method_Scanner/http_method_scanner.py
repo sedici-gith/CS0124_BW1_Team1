1ac44cffd762f60e28c39c09fca9ae980abb4c11
P1ZZO <157240502+P1ZZO@users.noreply.github.com>
2024-02-16 10:14:06 +0100

    Update http_method_scanner.py
    aggiunti dei commenti sui principali metodi del programma

--- Python_Method_Scanner/http_method_scanner.py
+++ Python_Method_Scanner/http_method_scanner.py
@@ -1,5 +1,5 @@
 import http.client
-def main():
+def main():           #rende metodo gli input da inserire 
     IP = input("inserire IP target: ")
     porta = input("inserire porta target (default: 80): ")
     percorso = input("inserire percorso (default: /): ")
@@ -12,13 +12,13 @@ def main():
         percorso = "/"
     scansione(IP, porta, percorso, metodi)
 
-def scansione(IP, porta, percorso, metodi):
+def scansione(IP, porta, percorso, metodi):    #rende metodo tutto quello scritto dopo
 	try:
 		for metodo in metodi:
-			connection = http.client.HTTPConnection(IP, porta)
-			connection.request(metodo, percorso)
-			risposta = connection.getresponse()
-			if risposta.status < 400:
+			connection = http.client.HTTPConnection(IP, porta)   #stabilisce una connessione con il percorso stabilito
+			connection.request(metodo, percorso)    #la connessione richiede che metodo e'attivo su quel percorso specifico
+			risposta = connection.getresponse()   #cattura la risposta dalla connessione
+			if risposta.status < 400:    
 			    print(f"metodo {metodo} abilitato, codice {risposta.status}")
 			else:
 			    print(f"metodo {metodo} disabilitato, codice {risposta.status}")
@@ -27,6 +27,6 @@ def scansione(IP, porta, percorso, metodi):
 		print("connessione fallita")
 		print(IP, "/", porta, "/", percorso)
     
-if __name__ == "__main__":
+if __name__ == "__main__":   #rende metodo tutto il codice
     main()
 
